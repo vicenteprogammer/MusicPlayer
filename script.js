9ad72855a4a3b23f8355cf1c9c31e1f1be46a5c3
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-03-31 10:56:38 -0300

    Day 3.

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ const song = document.getElementById("audio");
 const play = document.getElementById("play")
 
 songName.innerText = "O Pato";
+let isPlaying = false;
 
 function playSong(){
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
     song.play();
+    isPlaying = true;
 }
 
-play.addEventListener("click", playSong)
+function pauseSong(){
+    play.querySelector(".bi").classList.add("bi-play-circle-fill");
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }else{
+        playSong();
+    }
+}
+
+play.addEventListener("click", playPauseDecider);
